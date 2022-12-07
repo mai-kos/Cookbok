8f37b7cfcc47c6e7c89f0a2ac6c2d2c7ca25f7ca
Konstantin <maykos94@gmail.com>
2022-12-07 21:36:04 +0300

    Added a function to combine files

--- cookbook.py
+++ cookbook.py
@@ -38,3 +38,26 @@ def get_shop_list_by_dishes(dishes, person_count):
 
 pprint(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2))
 print()
+
+def count_lines(file):
+    with open(file, 'rt') as file:
+        x = len(file.readlines())
+        return x
+
+def combine_files():
+    len1, len2, len3 = count_lines('1.txt'), count_lines('2.txt'), count_lines('3.txt')
+    file_dict = {}
+    file_dict.update({len1: '1.txt', len2: '2.txt', len3: '3.txt'})
+    keys_list = sorted(list(file_dict.keys()))
+    with open('combined.txt', 'a') as document:
+        for el in keys_list:
+            document.write(file_dict.get(el) + '\n')
+            document.write(str(el) + '\n')
+            with open(file_dict.get(el), 'rt') as file:
+                document.writelines(line + '\n' for line in file.readlines())
+    return
+
+# Запись работает, но почему-то добавляется лишняя пустая строка
+# А если убрать из 57 строки '\n', то в итоге все наоборот склеивается
+
+combine_files()
